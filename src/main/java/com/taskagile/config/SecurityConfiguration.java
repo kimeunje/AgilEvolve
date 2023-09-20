719117e78a25f092e870d7ab8317a2619cff3ad6
kimeunje <world.wide.eunseo@gmail.com>
2023-09-20 13:16:14 +0900

    PasswordEncryptorDelegate 구현하기

--- src/main/java/com/taskagile/config/SecurityConfiguration.java
+++ src/main/java/com/taskagile/config/SecurityConfiguration.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -27,7 +29,7 @@ public class SecurityConfiguration {
   };
 
   @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
         .authorizeHttpRequests((requests) -> requests
             .requestMatchers(PUBLIC).permitAll()
@@ -41,5 +43,10 @@ public class SecurityConfiguration {
 
     return http.build();
 
+  }
+
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
--- src/main/java/com/taskagile/domain/common/security/PasswordEncryptorDelegator.java
+++ src/main/java/com/taskagile/domain/common/security/PasswordEncryptorDelegator.java
@@ -1,12 +1,18 @@
 package com.taskagile.domain.common.security;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PasswordEncryptorDelegator implements PasswordEncryptor {
+  private PasswordEncoder passwordEncoder;
+
+  public PasswordEncryptorDelegator(PasswordEncoder passwordEncoder) {
+    this.passwordEncoder = passwordEncoder;
+  }
+
   @Override
   public String encrypt(String rawPassword) {
-    // 이곳에 구현
-    return rawPassword;
+    return passwordEncoder.encode(rawPassword);
   }
 }
