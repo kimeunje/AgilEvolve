0f14f801477f8bbf75bb6991c2ca5702041ad7e2
kimeunje <world.wide.eunseo@gmail.com>
2023-09-27 18:56:40 +0900

    홈페이지 UI 추가하기

---
+++ front-end/src/components/PageHeader.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="page-header d-flex align-content-center">
+    <div class="logo">
+      <font-awesome-icon :icon="['fas', 'house']" class="home-icon" />
+      <img src="/static/images/logo.png">
+    </div>
+    <div class="boards-menu-toggle">
+      <div class="dropdown">
+        <button class="btn dropdown-toggle" type="button" id="boardsMenu" data-bs-toggle="dropdown" aria-haspopup="true"
+          aria-expanded="false">
+          Boards
+        </button>
+        <div class="dropdown-menu" aria-labelledby="boardsMenu">
+          <h6 class="dropdown-header">Personal Boards</h6>
+          <button class="dropdown-item" type="button">vuejs.spring-boot.mariadb</button>
+        </div>
+      </div>
+    </div>
+    <div class="search-box flex-fill">
+      <div class="search-wrapper">
+        <font-awesome-icon :icon="['fas', 'magnifying-glass']" class="search-icon" />
+        <input type="text" placeholder="Search" class="form-control form-control-sm" />
+      </div>
+    </div>
+    <div class="profile-menu-toggle">
+      <div class="dropdown">
+        <button class="btn dropdown-toggle" type="button" id="profileMenu" data-bs-toggle="dropdown" aria-haspopup="true"
+          aria-expanded="false">
+          k.eunseo
+        </button>
+        <div class="dropdown-menu" aria-labelledby="profileMenu">
+          <button class="dropdown-item" type="button">Profile</button>
+          <button class="dropdown-item" type="button">Sign Out</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.page-header {
+  padding: 9px 10px 8px;
+  border-bottom: 1px solid #eee;
+
+  .logo {
+    color: #444;
+    height: 25px;
+    width: 115px;
+    margin-top: 2px;
+    white-space: nowrap;
+
+    .home-icon {
+      font-size: 25px;
+      vertical-align: middle;
+    }
+
+    img {
+      margin-left: 5px;
+      width: 80px;
+      height: 20px;
+      vertical-align: bottom;
+    }
+  }
+
+  .boards-menu-toggle {
+    padding-left: 20px;
+    width: 100px;
+  }
+
+  .profile-menu-toggle {
+    width: 215px;
+    text-align: right;
+  }
+
+  .search-box {
+    .search-wrapper {
+      width: 400px;
+      margin: 0 auto;
+      position: relative;
+    }
+
+    .search-icon {
+      position: absolute;
+      top: 8px;
+      left: 8px;
+      color: #666;
+    }
+
+    input {
+      padding-left: 30px;
+      height: calc(1.8125rem + 5px);
+      font-size: 1rem;
+      border: 1px solid #eee;
+    }
+
+    input:focus {
+      border: 1px solid #377EF6;
+    }
+  }
+}
+
+.dropdown-toggle {
+  padding: 2px 5px !important;
+}
+</style>
--- front-end/src/main.ts
+++ front-end/src/main.ts
@@ -4,21 +4,28 @@ import axios from 'axios'
 
 import App from './App.vue'
 import router from './router'
-import 'bootstrap/dist/css/bootstrap.min.css'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUserSecret, faHouse, faSearch, faPlus } from '@fortawesome/free-solid-svg-icons'
 
 // axios 부트스트랩
 axios.defaults.baseURL = '/api'
 axios.defaults.headers.common.Accept = 'application/json'
 axios.interceptors.response.use(
-  response => response,
+  (response) => response,
   (error) => {
     return Promise.reject(error)
   }
 )
 
+library.add(faUserSecret, faHouse, faSearch, faPlus)
+
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
+app.component('font-awesome-icon', FontAwesomeIcon).use(createPinia()).use(router)
 
 app.mount('#app')
